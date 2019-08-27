f8188e5b2f56f69c9e1a9028ad117b8f7f622aa8
Tetano <viniciusvicco@gmail.com>
2019-08-27 17:14:39 -0300

    Configurando para o sql

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var app = require('./config/server')
 
+var rotaNoticias = require("./app/routes/noticias")//(app); // assim retorna execução da função
+rotaNoticias(app); // assim retorna a função + execução.
 
-app.get('/',function(req,res){
-    res.render("home/index.ejs")
-});
-app.get('/formulario_inclusao_noticia',function(req,res){
-    res.render("admin/form_add_noticia.ejs")
-});
-app.get('/noticias',function(req,res){
-    res.render("noticias/noticias")
-});
+var rotaHome = require("./app/routes/home")(app);
+
+
+var rotaFormInclusaoNoticia = require("./app/routes/furmulario_inclusao_noticia")(app);
+
+
+
 app.listen(3000, function(){
-    console.log("Servidor ON")
+    console.log("Servidor ON");
 });
