ce9a322f78fbdccca88db348e3fdcb30aaa8c8d4
Tetano <viniciusvicco@gmail.com>
2019-08-29 17:58:40 -0300

    Refectoring do metodo de conexão

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,16 +1,8 @@
+var dbConnection = require('../../config/dbConnection');
 module.exports = function(app){
 
-     app.get('/noticias', function(req,res){
- 
-         var mysql = require('mysql');
- 
-         var connection = mysql.createConnection({
-             host: 'localhost',
-             user: 'root',
-             password: '12345',
-             database: 'portal_noticias'
-         });
- 
+     app.get('/noticias', function(req,res){ 
+         var connection = dbConnection();
          connection.query("SELECT * FROM noticias", function(error, result){
              res.render("noticias/noticias",{noticias : result});
              console.log(error)
